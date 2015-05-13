6753b7c15144a1d9cdda427452f64cb8b0b28dfc
tpeang <tpeang@outlook.com>
2015-05-13 17:16:34 +0800

    新增第七章部分代码

--- src/com/example/tpapp/MainActivity.java
+++ src/com/example/tpapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.tpapp;
 
- 
-
 import com.example.tpapp.s2.*;
 import com.example.tpapp.s4.SelectBookActivity;
+import com.example.tpapp.s7.Blast;
+import com.example.tpapp.s7.Butterfly;
+import com.example.tpapp.s7.FatPo;
+import com.example.tpapp.s7.MatrixTest;
+import com.example.tpapp.s7.MeshTest;
+import com.example.tpapp.s7.ShaderTest;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -18,13 +22,15 @@ import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 public class MainActivity extends Activity {
-	Class<?>[] names = new Class[] { GridLayoutTest.class, CheckButtonTest.class,
-			ToggleButtonTest.class, ClockTest.class, ChronometerTest.class,
-			ImageViewTest.class, SimpleListViewTest.class,
-			ArrayAdapterTest.class, ExpandableListViewTest.class,
-			SearchViewTest.class, AlertDialogTest.class, DateDialogTest.class,
-			MenuTest.class, ContextMenuTest.class, ActionBar_TabNav.class ,PreferenceActivityTest.class
-			,SelectBookActivity.class};
+	Class<?>[] names = new Class[] { GridLayoutTest.class,
+			CheckButtonTest.class, ToggleButtonTest.class, ClockTest.class,
+			ChronometerTest.class, ImageViewTest.class,
+			SimpleListViewTest.class, ArrayAdapterTest.class,
+			ExpandableListViewTest.class, SearchViewTest.class,
+			AlertDialogTest.class, DateDialogTest.class, MenuTest.class,
+			ContextMenuTest.class, ActionBar_TabNav.class,
+			PreferenceActivityTest.class, SelectBookActivity.class,
+			MatrixTest.class, MeshTest.class, ShaderTest.class, FatPo.class,Blast.class ,Butterfly.class};
 
 	String[] arr = new String[] { names[0].getSimpleName(),
 			names[1].getSimpleName(), names[2].getSimpleName() };
@@ -43,7 +49,6 @@ public class MainActivity extends Activity {
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				R.layout.item_of_main, arr1);
 		gridView.setAdapter(adapter);
-	
 
 		gridView.setOnItemClickListener(new OnItemClickListener() {
 
@@ -54,6 +59,7 @@ public class MainActivity extends Activity {
 
 				System.out.println(names[position]);
 				Intent intent = new Intent(MainActivity.this, names[position]);
+
 				startActivity(intent);
 				Log.e("²éÕÒÌø×ªµÄActivity£º", "xia");
 
@@ -68,7 +74,7 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
- 
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
